434c075d8bd9bcb27a899f0a3143aafe0f94543d
BogdanMalashuk <kufar.120@gmail.com>
2025-06-16 18:44:06 +0300

    MenuItem and Menu registered in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import MenuItem
+from .models import MenuItem, Menu
 
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'menu_name', 'parent', 'named_url', 'url')
-    list_filter = ('menu_name',)
+    list_display = ('title', 'menu', 'parent', 'order')
+    list_filter = ('menu',)
+    ordering = ('menu', 'parent__id', 'order')
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name']
