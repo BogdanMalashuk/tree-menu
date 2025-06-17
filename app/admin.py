2db8b8f4aa544c12d9a8f9a218aa118f40db7016
BogdanMalashuk <kufar.120@gmail.com>
2025-06-17 14:03:13 +0300

    PEP8 & comments & docstrings

--- app/admin.py
+++ app/admin.py
@@ -4,11 +4,11 @@ from .models import MenuItem, Menu
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'menu', 'parent', 'order')
-    list_filter = ('menu',)
-    ordering = ('menu', 'parent__id', 'order')
+    list_display = ('title', 'menu', 'parent', 'order')  # Колонки в списке
+    list_filter = ('menu',)  # Фильтр по меню справа
+    ordering = ('menu', 'parent__id', 'order')  # Сортировка по меню, затем по родителю и порядку
 
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ['name']  # Отображение имени меню
