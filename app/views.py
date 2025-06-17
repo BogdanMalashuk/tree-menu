6b1ff85c6f0080a5cf7d129171531876627bca10
BogdanMalashuk <kufar.120@gmail.com>
2025-06-17 14:14:58 +0300

    PEP8 & comments & docstrings

--- app/views.py
+++ app/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from .models import Menu, MenuItem
-from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
+from typing import List, Dict, Any
+from django.http import HttpRequest, HttpResponse, Http404
 
 
-def build_menu_tree(menu_items):
+def build_menu_tree(menu_items: List[MenuItem]) -> List[Dict[str, Any]]:
+    """
+    Построить древовидную структуру меню из плоского списка пунктов.
+
+    Аргументы:
+        menu_items (List[MenuItem]): Список пунктов меню.
+
+    Возвращает:
+        List[Dict[str, Any]]: Список вложенных словарей, представляющих дерево меню.
+    """
     menu_dict = {}
     tree = []
 
@@ -30,7 +40,16 @@ def build_menu_tree(menu_items):
     return tree
 
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
+    """
+    Представление главной страницы.
+
+    Аргументы:
+        request (HttpRequest): Объект HTTP-запроса.
+
+    Возвращает:
+        HttpResponse: Ответ с отрендеренным шаблоном главной страницы.
+    """
     try:
         menu = Menu.objects.get(name='main_menu')
     except ObjectDoesNotExist:
@@ -42,9 +61,18 @@ def home(request):
     return render(request, 'main/home.html', {'menu': tree})
 
 
-def menu_page(request, slug):
-    path = '/' + slug.strip('/') + '/'
+def menu_page(request: HttpRequest, slug: str) -> HttpResponse:
+    """
+    Формирует полный путь с ведущим и завершающим слэшем, ищет пункт меню с совпадающим URL.
 
+    Аргументы:
+        request (HttpRequest): Объект HTTP-запроса.
+        slug (str): Часть URL, например 'about/team'.
+
+    Возвращает:
+        HttpResponse: Ответ с отрендеренным шаблоном страницы меню.
+    """
+    path = '/' + slug.strip('/') + '/'
     menu_item = MenuItem.objects.filter(url=path).first()
     if not menu_item:
         raise Http404("Page not found in menu")
