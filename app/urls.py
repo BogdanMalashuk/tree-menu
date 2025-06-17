3fc24b4f2d89e51dab190613e406ff9055cea950
BogdanMalashuk <kufar.120@gmail.com>
2025-06-17 14:07:44 +0300

    PEP8 & comments & docstrings

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    re_path(r'^(?P<slug>[-\w/]+)/$', menu_page, name='menu_page'),
+    re_path(r'^(?P<slug>[-\w/]+)/$', menu_page, name='menu_page'),  # Обработка вложенных URL-пунктов меню.
 ]
