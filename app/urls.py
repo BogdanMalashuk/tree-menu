dec93b073a513c4d8c7ff29e115e41256330a9bd
BogdanMalashuk <kufar.120@gmail.com>
2025-06-17 12:44:16 +0300

    item's checker url added

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import home
+from django.urls import path, re_path
+from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    re_path(r'^(?P<slug>[-\w/]+)/$', menu_page, name='menu_page'),
 ]
